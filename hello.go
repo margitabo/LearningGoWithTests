a9ed76001db1235a32261acdb4d0bb4c106de2d2
margitabo <margita.bo@gmail.com>
2022-05-31 11:35:35 +0300

    Add Spanish and French language

--- hello.go
+++ hello.go
@@ -3,14 +3,24 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishLanguage = "Spanish"
+const frenchLanguage = "French"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == spanishLanguage {
+		return spanishHelloPrefix + name
+	}
+	if language == frenchLanguage {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 
 }
--- hello_test.go
+++ hello_test.go
@@ -5,6 +5,8 @@ import "testing"
 func TestHello(t *testing.T) {
 
 	assertCorrectMessage := func(t testing.TB, got, want string) {
+		//t.Helper() is needed to tell the test suite that this method is a helper.
+		//By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
 		t.Helper()
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
@@ -12,17 +14,29 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Luis", "French")
+		want := "Bonjour, Luis"
+		assertCorrectMessage(t, got, want)
 	})
 }
