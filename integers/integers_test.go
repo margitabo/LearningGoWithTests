5c6eaba25bd5e502b4f49db4cc6bae6830f50e6c
margitabo <margita.bo@gmail.com>
2022-06-01 15:14:52 +0300

    Add integers and integers_tests

---
+++ integers/integers_test.go
@@ -0,0 +1,28 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+//Please note that the example function will not be executed if you remove the comment // Output: 6.
+//Although the function will be compiled, it won't be executed.
+//By adding this code the example will appear in the documentation inside godoc, making your code even more accessible.
+//To try this out, run godoc -http=:6060 and navigate to http://localhost:6060/pkg/
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
+}
+
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+
+	if sum != expected {
+		//we're using %d as our format strings rather than %q.
+		//That's because we want it to print an integer rather than a string.
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+
+}
