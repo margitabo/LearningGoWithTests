e53ee83704c0f2a1c061856b983ba10bf5f65713
margitabo <margita.bo@gmail.com>
2022-06-10 15:39:35 +0300

    Add Rectangle and Circle structs and tests

--- structs-and-interfaces/structs.go
+++ structs-and-interfaces/structs.go
@@ -1,14 +1,25 @@
 package structs_and_interfaces
 
+import "math"
+
+const pi = 3.14
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+type Circle struct {
+	Radius float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return pi * math.Pow(c.Radius, c.Radius)
+}
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
--- structs-and-interfaces/structs_test.go
+++ structs-and-interfaces/structs_test.go
@@ -5,11 +5,21 @@ import "testing"
 func TestAreas(t *testing.T) {
 	t.Run("rectanglesArea", func(t *testing.T) {
 		rectangle := Rectangle{12, 6}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
-			t.Errorf("got %g want %g", got, want)
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("circleArea", func(t *testing.T) {
+		circle := Circle{2}
+		got := circle.Area()
+		want := 12.56
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
 		}
 	})
 }
