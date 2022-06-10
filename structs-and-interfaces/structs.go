ec1dd14f9860c22501fb575658f4d7b5172b7388
margitabo <margita.bo@gmail.com>
2022-06-10 16:13:28 +0300

    Add Shape interface

--- structs-and-interfaces/structs.go
+++ structs-and-interfaces/structs.go
@@ -2,8 +2,6 @@ package structs_and_interfaces
 
 import "math"
 
-const pi = 3.14
-
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -13,13 +11,24 @@ type Circle struct {
 	Radius float64
 }
 
+//In Go interface resolution is implicit. If the type you pass in matches what the interface is asking for, it will compile.
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
 func (c Circle) Area() float64 {
-	return pi * math.Pow(c.Radius, c.Radius)
+	return math.Pi * math.Pow(c.Radius, c.Radius)
 }
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
 }
--- structs-and-interfaces/structs_test.go
+++ structs-and-interfaces/structs_test.go
@@ -3,35 +3,39 @@ package structs_and_interfaces
 import "testing"
 
 func TestAreas(t *testing.T) {
-	t.Run("rectanglesArea", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		got := rectangle.Area()
-		want := 72.0
-
+	/*Decoupling
+	Notice how our helper does not need to concern itself with whether the shape is a Rectangle or a Circle or a Triangle. By declaring an interface, the helper is decoupled from the concrete types and only has the method it needs to do its job.
+	This kind of approach of using interfaces to declare only what you need is very important in software design and will be covered in more detail in later sections.
+	*/
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
-	t.Run("circleArea", func(t *testing.T) {
+	t.Run("circle", func(t *testing.T) {
 		circle := Circle{2}
-		got := circle.Area()
-		want := 12.56
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkArea(t, circle, 12.566370614359172)
 	})
 }
 
 func TestPerimeters(t *testing.T) {
-	t.Run("rectanglesPerimeter", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := Perimeter(rectangle)
-		want := 40.0
-
+	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Perimeter()
 		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+			t.Errorf("got %g want %g", got, want)
 		}
+	}
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkPerimeter(t, rectangle, 40.0)
 	})
 }
